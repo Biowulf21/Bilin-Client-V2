6c548ac36819270230e03647952e510d47b01900
James Jilhaney <20190016830@my.xu.edu.ph>
2022-06-29 11:49:19 +0800

    Fixed navbar layout

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { LoginBtn } from "./sub-components/login_btn.tsx";
 
 const NavBar = () =>{
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to={'/'}>Bilin - The Food Donation App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
-            <Nav.Link as={Link} to={'/get-involved'}>Get Involved</Nav.Link>
-            <Nav.Link as={Link} to={'/knowledge-hub'}>Knowledge Hub</Nav.Link>
-            <Nav.Link as={Link} to={'/about-us'}>About Us</Nav.Link>
-            <Nav.Link as={Link} to={'/faq'}>FAQ</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+           <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+  <Container>
+  <Navbar.Brand href="/">Bilin - The Food Donation App</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="m-auto">
+   <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
+   <Nav.Link as={Link} to={'/get-involved'}>Get Involved</Nav.Link>
+   <Nav.Link as={Link} to={'/knowledge-hub'}>Knowledge Hub</Nav.Link>
+   <Nav.Link as={Link} to={'/about-us'}>About Us</Nav.Link>
+   <Nav.Link as={Link} to={'/faq'}>FAQ</Nav.Link>
+      
+    </Nav>
+    <Nav>
+<LoginBtn className="justify-content-end"></LoginBtn>      
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
         </div>
     );
 }
---
+++ src/components/navbar/sub-components/login_btn.tsx
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+
+
+    const LoginBtn = () => {
+        return (
+            <div>
+                <Button variant="outline-dark">Log In</Button>
+            </div>
+        );
+    }
+
+    export {LoginBtn};
